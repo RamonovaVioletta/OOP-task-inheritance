1e8b9b03b323f07cd1e62afb9a51bf7b7641da2e
RamonovaVioletta <163902400+RamonovaVioletta@users.noreply.github.com>
2024-04-04 17:27:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from numpy import average
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -18,6 +21,31 @@ class Student:
         else:
             return 'Ошибка'
 
+    def get_average_grade(self):
+        if self.grades:
+            return average(list(self.grades.values()))
+        else:
+            return 0
+
+    def __str__(self):
+        avg_grade = self.get_average_grade()
+        courses_in_progress = ', '.join(self.courses_in_progress) or 'Нет'
+        finished_courses = ', '.join(self.finished_courses) or 'Нет'
+        return f'''
+        === Student ===
+        Имя: {self.name}
+        Фамилия: {self.surname}
+        Средняя оценка за домашние задания: {avg_grade}
+        Курсы в процессе изучения: {courses_in_progress}
+        Завершенные курсы: {finished_courses}
+        '''
+
+    def __lt__(self, another_student):
+        if isinstance(another_student, Student):
+            return self.get_average_grade() < another_student.get_average_grade()
+        else:
+            return 'Ошибка. Сравнивать можно только с другим студентом'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,6 +58,27 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def get_average_grade(self):
+        if self.grades:
+            return average(list(self.grades.values()))
+        else:
+            return 0
+
+    def __str__(self):
+        avg_grade = self.get_average_grade()
+        return f'''
+        === Lecturer ===
+        Имя: {self.name}
+        Фамилия: {self.surname}
+        Средняя оценка за лекции: {avg_grade}
+        '''
+
+    def __lt__(self, another_lecturer):
+        if isinstance(another_lecturer, Lecturer):
+            return self.get_average_grade() < another_lecturer.get_average_grade()
+        else:
+            return 'Ошибка. Сравнивать можно только с другим лекторами'
 
 
 class Reviewer(Mentor):
@@ -44,6 +93,13 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'''
+        === Reviewer ===
+        Имя: {self.name}
+        Фамилия: {self.surname}
+        '''
 
 
 student_1 = Student('Alla', 'Gurina', 'female')
@@ -62,15 +118,19 @@ reviewer_1 = Reviewer('Nik', 'Kim')
 reviewer_1.courses_attached += ['Python']
 
 student_1.rate_to_lec(lecturer_1, 'Python', 10)
-student_1.rate_to_lec(lecturer_1, 'Python', 10)
+student_1.rate_to_lec(lecturer_1, 'Python', 7)
 
-student_1.rate_to_lec(lecturer_2, 'Python', 10)
-student_1.rate_to_lec(lecturer_2, 'Python', 10)
+student_1.rate_to_lec(lecturer_2, 'Python', 5)
+student_1.rate_to_lec(lecturer_2, 'Python', 6)
 
 
 reviewer_1.rate_to_std(student_1, 'Python', 10)
-reviewer_1.rate_to_std(student_1, 'Python', 10)
-reviewer_1.rate_to_std(student_2, 'Python', 10)
+reviewer_1.rate_to_std(student_1, 'Python', 2)
+reviewer_1.rate_to_std(student_2, 'Python', 9)
 
-print(student_1.grades)
-print(lecturer_2.grades)
+
+print(reviewer_1)
+print(lecturer_2)
+print(student_1)
+
+
